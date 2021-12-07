793eef6beaf428328c352e5217ae6b28948c75ad
codechirag5610 <80476092+codechirag5610@users.noreply.github.com>
2021-12-07 16:17:31 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,25 @@
-function compute()
-{
-    p = document.getElementById("principal").value;
-    
+function compute(){
+    var p = document.getElementById("principal").value;
+    var q = document.getElementById("rate").value;
+    var r = document.getElementById("years").value;
+    var res = (p*q*r)/100;
+    var year = new Date().getFullYear() + parseInt(r);
+    if(p == ""){
+        alert("Please enter a principal value");
+        document.getElementById("principal").focus();
+    }
+    if(p <= 0){
+      alert("Please enter a positive principal value");
+      document.getElementById("principal").focus();
+    }
+    document.getElementById("result").innerHTML = " If you deposit " + p + " at an interest rate of " + q + " you will get " + res + " in year " + year;
 }
-        
+
+
+var slider = document.getElementById("rate");
+var output = document.getElementById("rate_val");
+output.innerHTML = slider.value + "%";
+slider.oninput = function() {
+    output.innerHTML = this.value + "%";
+}
+    
